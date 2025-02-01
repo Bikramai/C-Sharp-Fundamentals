138c40d85c4b61f79bca49617e18503d0331ba26
bikram <bikra@BIKRAM-LENOVO>
2025-02-01 13:00:56 -0500

    Primitive Types and Expressions-by using var

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@ namespace Variables
         {
             static void Main(string[] args)
             {
-                byte number = 2;
-                int count = 10;
-                float totalPrice = 20.95f;
-                char character = 'A';
-                string firstName = "Bikram";
-                bool isWorking = true;
+                var number = 2;
+                var count = 10;
+                var totalPrice = 20.95f;
+                var character = 'A';
+                var firstName = "Bikram";
+                var isWorking = true;
 
                 Console.WriteLine(number);
                 Console.WriteLine(count);
