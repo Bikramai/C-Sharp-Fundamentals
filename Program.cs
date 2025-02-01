d3a6cb08db7ccf9091fb4690ffdff3f898c085b9
bikram <bikra@BIKRAM-LENOVO>
2025-02-01 14:30:50 -0500

    How to handle the Execption-to solve wrap the block of code with try catch

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            var number = "1234";
-            byte b = Convert.ToByte(number);
-            Console.WriteLine(b);
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
         }
     }
 
