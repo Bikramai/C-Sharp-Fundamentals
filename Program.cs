13a6546a912ed51a384e2cb5dc1f70a559c48045
bikram <bikra@BIKRAM-LENOVO>
2025-02-01 12:38:11 -0500

    Primitives Types and Expressions -Variables

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 
 
-namespace Hello_World
+namespace Variables
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            namespace Variables
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                byte number = 2;
+                int count = 10;
+                float totalPrice = 20.95f;
+                char character = 'A';
+                string firstName = "Bikram";
+                bool isWorking = true;
+
+                Console.WriteLine(number);
+                Console.WriteLine(count);
+                Console.WriteLine(totalPrice);
+                Console.WriteLine(character);
+                Console.WriteLine(firstName);
+                Console.WriteLine(isWorking);
+            }
         }
+
     }
+}
 }
