9e7edec948b7be60568edf315ed36ee57bf27a92
bikram <bikra@BIKRAM-LENOVO>
2025-02-02 15:55:40 -0500

    Arrays -Int, boolean, string

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
-using Hello_World.Math;
 
-namespace Classes
+namespace Arrays
 {
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            var Bikram = new Person();
-            Bikram.FirstName = "Bikram";
-            Bikram.LastName = "Limbu";
-            Bikram.Introduce();
+            // Array of Int
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            // Array of boolean
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            // array of string
+            var nanes = new string[3] { "Ikram", "ayusha", "Mangi" };
 
         }
     }
