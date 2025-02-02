d0454ad24d43744c496cac6abdf81b0cc0db9c04
bikram <bikra@BIKRAM-LENOVO>
2025-02-02 11:34:58 -0500

    Operators -comparsion operators

--- Program.cs
+++ Program.cs
@@ -11,11 +11,14 @@ namespace Operators
         {
             var a = 1;
             var b = 2;
-            var c = 3;
+            
 
-            Console.WriteLine(a + b * c);
-            Console.WriteLine((a + b) * c);
-
+            Console.WriteLine(a > b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a >= b);
 
         }
     }
